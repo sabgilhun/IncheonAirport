26e3805a2cb94a7cd92908320ca2d09764961a62
sabgilhun <sabgilhun@gmail.com>
2019-03-10 22:56:31 +0900

    DataBinding 설정

--- app/src/main/java/ado/sabgil/incheonariport/MainActivity.java
+++ app/src/main/java/ado/sabgil/incheonariport/MainActivity.java
@@ -1,14 +1,18 @@
 package ado.sabgil.incheonariport;
 
+import ado.sabgil.incheonariport.databinding.ActivityMainBinding;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding mBinding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
     }
 }
