b2876cb73b423cf670812974f5775f6e4b68218c
sabgilhun <sabgilhun@gmail.com>
2019-03-13 11:38:31 +0900

    toolbar, search view 추가

--- app/src/main/java/ado/sabgil/incheonariport/MainActivity.java
+++ app/src/main/java/ado/sabgil/incheonariport/MainActivity.java
@@ -2,7 +2,10 @@ package ado.sabgil.incheonariport;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import androidx.appcompat.widget.SearchView;
 
 import ado.sabgil.incheonariport.databinding.ActivityMainBinding;
 import ado.sabgil.incheonariport.remote.openapi.IncheonAirportApiHandler;
@@ -19,13 +22,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        setSupportActionBar(mBinding.tbMain);
         handler = IncheonAirportApiHandler.getInstance();
 
-        mBinding.btnWeather.setOnClickListener(__ ->
-                handler.getDeparturesWeather(mBinding.etWeather.getText().toString().toUpperCase(),
-                        response -> mBinding.tvWeather.setText(response),
-                        error -> Log.e("Main", error.getMessage()))
-        );
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) searchItem.getActionView();
+        searchView.onActionViewCollapsed();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+//                handler.getDeparturesWeather(query.toUpperCase(),
+//                        response -> mBinding.tvWeather.setText(response),
+//                        error -> Log.e("Main", error.getMessage()));
+
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return true;
+            }
+        });
+
+        return true;
     }
 
     public void onClickUpdateCongestion(View v) {
