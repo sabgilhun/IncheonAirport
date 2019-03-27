835a9fdf72e0c9882c598bb307d93dd8377b5bea
sabgilhun <sabgilhun@gmail.com>
2019-03-27 12:04:31 +0900

    EditText에 debouce 기능 로직 구현

--- app/src/main/java/ado/sabgil/incheonariport/view/PlaneSearchActivity.java
+++ app/src/main/java/ado/sabgil/incheonariport/view/PlaneSearchActivity.java
@@ -1,20 +1,99 @@
 package ado.sabgil.incheonariport.view;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 import ado.sabgil.incheonariport.R;
+import ado.sabgil.incheonariport.adapter.FlightInfoAdapter;
+import ado.sabgil.incheonariport.data.DataManager;
+import ado.sabgil.incheonariport.data.DataManagerImpl;
 import ado.sabgil.incheonariport.databinding.ActivityPlaneSearchBinding;
 import ado.sabgil.incheonariport.view.base.BaseActivity;
 import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class PlaneSearchActivity extends BaseActivity<ActivityPlaneSearchBinding> {
 
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+    private DataManager dataManager;
+    private Timer debounceTimer;
+    public static int DEBOUNCE_TIMER_DURATION = 500;
+    public static int SHORT_TEXT_CRITERION = 2;
 
     protected int getLayout() {
         return R.layout.activity_plane_search;
     }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // DataManager 초기화
+        dataManager = DataManagerImpl.getInstance();
+
+        // view 초기화
+        getBinding().ivBackButton.setOnClickListener(__ -> finish());
+        getBinding().ivClear.setOnClickListener(__ -> getBinding().etSearch.setText(null));
+        getBinding().etSearch.addTextChangedListener(textWatcher);
+        initRecyclerView();
+    }
+
+    private void initRecyclerView() {
+        RecyclerView recyclerView = getBinding().rvSearchedItem;
+        FlightInfoAdapter adapter = new FlightInfoAdapter();
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void loadFlightData(String query) {
+        if (query.length() < SHORT_TEXT_CRITERION) {
+            runOnUiThread(() ->
+                    getBinding().flListContainer.bringChildToFront(getBinding().lyShortTextScreen));
+            return;
+        }
+
+        dataManager.getSimpleFlightInfoWithId(query.toUpperCase(),
+                result -> {
+                    getBinding().setItems(result);
+                    getBinding().flListContainer.bringChildToFront(getBinding().lyListScreen);
+                },
+                error -> Log.e("networking", error.getMessage())
+        );
+    }
+
+    private TextWatcher textWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            if (debounceTimer != null) {
+                debounceTimer.cancel();
+            }
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            if (s.length() > 0) {
+                getBinding().ivClear.setVisibility(View.VISIBLE);
+            } else {
+                getBinding().ivClear.setVisibility(View.INVISIBLE);
+                getBinding().flListContainer.bringChildToFront(getBinding().lyDefaultScreen);
+            }
+
+            debounceTimer = new Timer();
+            debounceTimer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    loadFlightData(s.toString());
+                }
+            }, DEBOUNCE_TIMER_DURATION);
+        }
+    };
+
 }
